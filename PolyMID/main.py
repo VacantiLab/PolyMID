df2de4df1afe87ffd35f1dccabf485ac9a3ae325
Nathaniel Vacanti <nate@Nathaniels-iMac.local>
2020-04-20 16:01:46 -0400

    small change

--- PolyMID/main.py
+++ PolyMID/main.py
@@ -15,13 +15,8 @@ def main(fragment=None,TextFile=None):
 
     #create a Fragment object if one was not passed to this function
     if (fragment is None):
-        #initialize fragment object attributes not taken as function inputs
-        MIDc=None
-        PeakArea=None
-
         #creates a fragment object
-        fragment = Fragment(formula=Inputs.formula, MetaboliteAtoms=Inputs.MetaboliteAtoms, MIDu=Inputs.MIDu, AtomLabeled=Inputs.AtomLabeled, FragmentName=Inputs.FragmentName, CM=Inputs.CM, MIDc=MIDc, PeakArea=PeakArea)
-
+        fragment = Fragment(formula=Inputs.formula, MetaboliteAtoms=Inputs.MetaboliteAtoms, MIDu=Inputs.MIDu, AtomLabeled=Inputs.AtomLabeled, FragmentName=Inputs.FragmentName, CM=Inputs.CM, MIDc=None, PeakArea=None)
 
     #create a correction matrix if one is not provided in the Fragment object
     if fragment.CM is None:
