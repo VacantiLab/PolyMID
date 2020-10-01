2cc1717e4e1e815f314be44b157fe4656ef44c0d
Nathaniel Vacanti <nate@dhcp-vl2042-9600.redrover.cornell.edu>
2020-10-01 09:42:51 -0400

    made Correct with capital C in commands file

--- references/commands.py
+++ references/commands.py
@@ -2,4 +2,4 @@
 #Run this from the directory containing the Pesciolini folder
 import PolyMID
 TextFile = '/Users/nate/git_hub_projects/PolyMID/references/ExampleInputWithoutCM.txt'
-PolyMID.correct(TextFile=TextFile)
+PolyMID.Correct(TextFile=TextFile)
