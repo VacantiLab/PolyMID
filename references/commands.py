df33eed01b9f25fbf18434a0fac1eacdae101d0e
Nathaniel Vacanti <nate@dhcp-vl2042-9600.redrover.cornell.edu>
2020-10-12 15:50:59 -0400

    changed commands file

--- references/commands.py
+++ references/commands.py
@@ -2,4 +2,4 @@
 #Run this from the directory containing the Pesciolini folder
 import PolyMID
 TextFile = '/Users/nate/git_hub_projects/PolyMID/references/ExampleInput.txt'
-PolyMID.Correct(CorrectInput=TextFile)
+f = PolyMID.Correct(CorrectInput=TextFile)
