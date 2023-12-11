a06c51009786238814ae4d33157b61cf687a7a56
Nathaniel Vacanti <nate@dhcp-vl2042-12268.redrover.cornell.edu>
2023-12-11 12:29:50 -0500

    fixed the shell script for plotting to add the grandparent directory to the python path

--- AnalyzeSpectra/PlotCDF.sh
+++ AnalyzeSpectra/PlotCDF.sh
@@ -3,11 +3,13 @@
 # retrieve the directory of the bokey script
 #   it is in the same directory as this shell script
 script_dir="$(dirname "$0")"
+parent_dir="$(dirname "$script_dir")"
+grandparent_dir="$(dirname "$parent_dir")"
 script_file="${script_dir}/PlotCDF.py"
 
 # activate the conda environment
 source activate gcms
-export PYTHONPATH="${PYTHONPATH}:/Users/nate/git_hub_tools"
+export PYTHONPATH="${PYTHONPATH}:${grandparent_dir}"
 
 # Find a port that is not in use
 port=5006
