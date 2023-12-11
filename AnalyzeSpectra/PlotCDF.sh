891da377895b89eef85c51317664d6d274417986
Nathaniel Vacanti <nate@dhcp-vl2042-12268.redrover.cornell.edu>
2023-12-11 12:08:53 -0500

    changed the PlotCDF shell script to find an unused port

--- AnalyzeSpectra/PlotCDF.sh
+++ AnalyzeSpectra/PlotCDF.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-source activate gcms10
-export PYTHONPATH="${PYTHONPATH}:/Users/nate/git_hub_projects"
+# retrieve the directory of the bokey script
+#   it is in the same directory as this shell script
+script_dir="$(dirname "$0")"
+script_file="${script_dir}/PlotCDF.py"
 
-bokeh serve --show PlotCDF.py --port $1
+# activate the conda environment
+source activate gcms
+export PYTHONPATH="${PYTHONPATH}:/Users/nate/git_hub_tools"
+
+# Find a port that is not in use
+port=5006
+# increment the port number until we find an available one
+while true; do
+  if lsof -Pi :$port -sTCP:LISTEN -t >/dev/null ; then
+    port=$((port+1))
+  else
+    break
+  fi
+done
+
+# run the bokeh server
+bokeh serve --show "$script_file" --port $port
